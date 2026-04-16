3f8ec4c1f775eb5d6a41f60d1cab37c7ea599a5f
Shihab Uddin <shihabuddin1160@gmail.com>
2026-04-16 23:57:21 +0600

    add user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $users = array(
+            array('name' => 'Shihab Uddin', 'email' => 'shihabuddin@gmail.com', 'password' => Hash::make('ShihabUddin@786'), 'created_at' => now(), 'updated_at' => now())
+        );
+
+        User::insert($users);
+    }
+}
