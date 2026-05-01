58df1e670eb0bfcde3896b9f358fe9fdc7f587cd
Shihab Uddin <shihabuddin1160@gmail.com>
2026-05-01 12:37:57 +0600

    update footer

--- resources/views/layouts/web/components/footer.blade.php
+++ resources/views/layouts/web/components/footer.blade.php
@@ -7,6 +7,7 @@
                     <li><i class="fa-solid fa-location-dot me-1"></i><span class="text-secondary">Farmgate, Dhaka, Bangladesh</span></li>
                     <li><i class="fa-solid fa-phone me-1"></i><span class="text-secondary">+880 1310-391409</span></li>
                     <li><i class="fa-solid fa-envelope me-1"></i><span class="text-secondary">shihabuddin1160@gmail.com</span></li>
+                    <li><i class="fa-solid fa-business-time me-1"></i><span class="text-secondary">Sat - Thu: 8:00 AM - 5:00 PM</span></li>
                 </ul>
             </div>
             <div class="col-lg-4 col-md-6">
@@ -14,13 +15,13 @@
                 <ul class="footer-list">
                     <li><a href="#" class="text-secondary">Home</a></li>
                     <li><a href="#" class="text-secondary">About</a></li>
-                    <li><a href="#" class="text-secondary">Privacy & Policy</a></li>
+                    <li><a href="#" class="text-secondary">Privacy Policy</a></li>
                     <li><a href="#" class="text-secondary">Terms & Conditions</a></li>
                 </ul>
             </div>
             <div class="col-lg-4 col-md-12">
                 <h5 class="footer-title fw-bolder fs-5">Social Media</h5>
-                <p class="text-secondary social-note">Follow me on these social media for the latest updates and news.</p>
+                <p class="text-secondary social-note">Stay connected on my social media for the latest updates, tech tips, and new contents.</p>
                 <div class="footer-social">
                     <a href="#" target="_blank"><i class="fab fa-facebook fs-6"></i></a>
                     <a href="#" target="_blank"><i class="fab fa-instagram fs-6"></i></a>
@@ -30,7 +31,7 @@
             </div>
         </div>
         <div class="footer-bottom text-center mt-4">
-            <p class="fw-bold text-secondary">© 2026 Shihab Uddin. All rights reserved.</p>
+            <p class="text-secondary">© 2026 Shihab Uddin. All rights reserved.</p>
         </div>
     </div>
 </footer>
