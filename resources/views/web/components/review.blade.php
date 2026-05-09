0595016b7444b04d6ca4dc9ba4a65efbf68f09bc
Shihab Uddin <shihabuddin1160@gmail.com>
2026-05-09 20:52:28 +0600

    working on review modal

--- resources/views/web/components/review.blade.php
+++ resources/views/web/components/review.blade.php
@@ -1,20 +1,22 @@
 <div class="modal fade review-modal" id="reviewModal" tabindex="-1" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-md">
         <div class="modal-content border-0 shadow-lg rounded-4">
-            <div class="modal-header border-0 pb-0">
-                <h4 class="modal-title fw-bold">Review Me</h4>
-                <button type="button" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div class="modal-header border-1">
+                <h5 class="modal-title fw-bold"><i class="fa-solid fa-star"></i>Share Your Experience</h5>
+                <button type="button" class="custom-close-btn" data-bs-dismiss="modal" aria-label="Close">
+                    <i class="fa-solid fa-xmark fs-5"></i>
+                </button>
             </div>
             <div class="modal-body pt-3">
                 <form action="" method="POST">
                     <div class="row g-3">
                         <div class="col-md-6">
-                            <label class="form-label fw-semibold">Full Name</label>
+                            <label class="form-label fw-semibold">Full Name <span class="text-danger">*</span></label>
                             <input type="text" class="form-control custom-input" placeholder="Enter your full name">
                         </div>
 
                         <div class="col-md-6">
-                            <label class="form-label fw-semibold">Email Address</label>
+                            <label class="form-label fw-semibold">Email Address <span class="text-danger">*</span></label>
                             <input type="email" class="form-control custom-input" placeholder="Enter email address">
                         </div>
 
@@ -29,7 +31,7 @@
                         </div>
 
                         <div class="col-12">
-                            <label class="form-label fw-semibold d-block"> Rating</label>
+                            <label class="form-label fw-semibold d-block"> Rating <span class="text-danger">*</span></label>
                             <div class="rating-stars">
                                 <input type="radio" name="rating" id="star5">
                                 <label for="star5">★</label>
@@ -50,9 +52,9 @@
                     </div>
                 </form>
             </div>
-            <div class="modal-footer border-0 pt-0">
-                <button type="button" class="btn btn-light px-4" data-bs-dismiss="modal">Close</button>
-                <button type="submit" class="btn btn-primary px-4">Submit</button>
+            <div class="modal-footer d-flex justify-content-center border-0 pt-0">
+                <button type="button" class="btn btn-outline-secondary px-4" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn custom-btn px-4">Submit</button>
             </div>
         </div>
     </div>
