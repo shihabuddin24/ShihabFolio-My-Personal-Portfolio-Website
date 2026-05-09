dad232657009972c74baee376c000143eeef24e1
Shihab Uddin <shihabuddin1160@gmail.com>
2026-05-09 21:46:39 +0600

    complete review modal

--- resources/views/web/components/review.blade.php
+++ resources/views/web/components/review.blade.php
@@ -31,22 +31,22 @@
                         </div>
 
                         <div class="col-12">
-                            <label class="form-label fw-semibold d-block"> Rating <span class="text-danger">*</span></label>
+                            <label class="form-label fw-semibold mb-2"> Rating <span class="text-danger">*</span></label>
                             <div class="rating-stars">
-                                <input type="radio" name="rating" id="star5">
-                                <label for="star5">★</label>
+                                <input type="radio" name="rating" id="star5" value="5">
+                                <label for="star5"><i class="fa-regular fa-star"></i></label>
 
-                                <input type="radio" name="rating" id="star4">
-                                <label for="star4">★</label>
+                                <input type="radio" name="rating" id="star4" value="4">
+                                <label for="star4"><i class="fa-regular fa-star"></i></label>
 
-                                <input type="radio" name="rating" id="star3">
-                                <label for="star3">★</label>
+                                <input type="radio" name="rating" id="star3" value="3">
+                                <label for="star3"><i class="fa-regular fa-star"></i></label>
 
-                                <input type="radio" name="rating" id="star2">
-                                <label for="star2">★</label>
+                                <input type="radio" name="rating" id="star2" value="2">
+                                <label for="star2"><i class="fa-regular fa-star"></i></label>
 
-                                <input type="radio" name="rating" id="star1">
-                                <label for="star1">★</label>
+                                <input type="radio" name="rating" id="star1" value="1">
+                                <label for="star1"><i class="fa-regular fa-star"></i></label>
                             </div>
                         </div>
                     </div>
